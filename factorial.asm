d85dd1b3f10d655b44171a8421c3f2b1c6e7c7b0
Sk Hasibul Alam <sk.hasibul.alam@gmail.com>
2021-10-06 20:45:59 +0600

    Create factorial.asm

---
+++ factorial.asm
@@ -0,0 +1,19 @@
+CODE SEGMENT
+ASSUME CS : CODE ., DS : CODE
+ORG 100H
+MAIN :  MOV AX, CS
+        MOV DS, AX
+        MOV AX, 1
+        MOV BX, 0
+        MOV DX, 0
+        MOV CX, 5
+
+KORO :  INC BX
+        MUL BX
+        CMP BX, CX
+        JNE KORO
+
+MOV AH, 4CH
+INT 21H
+CODE ENDS
+END MAIN
